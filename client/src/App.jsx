e80826b9133c0d582bb0207ef8c2095566f29775
sabarish <vbsabarishsriram@gmail.com>
2024-07-27 13:44:12 +0530

    feat: react flow hero

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Flow from './components/Flow'
 function App() {
   return (
-    <div className='h-full w-full'>
+    <div className='h-full w-full '>
       <Navbar 
         logoName="Nexus"
         buttonText="About"
       />
-      <div className='flex h-full items-center  border justify-center'>
+      <div className='flex h-full items-center px-10  justify-center'>
         <Hero
           heading1="Wire Your Ideas with "
           heading2='Nexus'
           description='A tool for visualizing and navigating complex project structures and files.'
           button3='Get Started'
+          className="w-2/5 h-full bg-white "
         />
+        <Flow
+          className='w-3/5'/>
       </div>
     </div>
   )
