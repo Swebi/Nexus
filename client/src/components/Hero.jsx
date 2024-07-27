153f897241616d1bb136010c5904ae6149d97a5d
suhayb <iamsuhaybahmed@gmail.com>
2024-07-27 14:51:34 +0530

    feat: add router, dashboard & layout

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
 
-function Hero({button3,heading1, heading2, description}) {
+function Hero({ buttonText, heading1, heading2, description }) {
   return (
-    <div className=' h-full flex flex-col '>
-        <div>
-            <span className='font-sans font-semibold  tracking-tighter text-6xl'>{heading1} <span className='text-[#FF338F]'>{heading2}</span></span>
-        </div>
-        <div className='mt-3'>
-            <p className='font-normal text-lg leading-8'>{description}</p>
-        </div>
-        <div className='mt-3'>
-            <button className='bg-[#FF338F] text-white poppins-medium px-7 py-2 rounded-full shadow-lg'>{button3}</button>
-        </div>
+    <div className=" h-full flex flex-col mx-12 justify-center items-center ">
+      <div>
+        <span className="font-sans font-semibold  tracking-tighter text-6xl">
+          {heading1} <span className="text-[#FF338F]">{heading2}</span>
+        </span>
+      </div>
+      <div className="mt-3">
+        <p className="font-normal text-lg leading-8">{description}</p>
+      </div>
+      <div className="mt-3 self-start">
+        <a href="/dashboard">
+          <button className="bg-[#FF338F] text-white poppins-medium px-7 py-2 rounded-full shadow-lg">
+            {buttonText}
+          </button>
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
