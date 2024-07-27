2644e489b338efb950aab1c60905bb4b96c00795
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-07-27 14:47:16 +0530

    feat: handle folder route

---
+++ server/src/api/folder/folder.controller.ts
@@ -0,0 +1,32 @@
+import {
+  type NextFunction,
+  type Request,
+  type Response,
+  Router,
+} from 'express';
+import { MESSAGES } from '../../shared/errors';
+import { getDirectoryStructure } from './folder.service';
+
+export const handleDirectoryStructure = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { directoryPath } = req.body;
+  try {
+    const structure = await getDirectoryStructure(directoryPath);
+    res.status(200).json({
+      success: true,
+      message: MESSAGES.DIRECTORY_SUCCESS,
+      structure,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default (): Router => {
+  const app = Router();
+  app.post('/directory-structure', handleDirectoryStructure);
+  return app;
+};
--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import folderController from './folder/folder.controller';
 import testController from './test/test.controller';
 
 export default (): Router => {
   const app = Router();
   app.use('/test', testController());
+  app.use('/folder', folderController());
   return app;
 };
