936bbc3b916d5c53a7831083ff8ceb36cd29ea9d
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-07-27 15:28:05 +0530

    chore: remove uri

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -5,7 +5,6 @@ dotenv.config();
 
 const envSchema = z.object({
   PORT: z.string(),
-  MONGODB_URI: z.string(),
   NODE_ENV: z.string(),
 });
 
@@ -14,7 +13,6 @@ export type EnvSchemaType = z.infer<typeof envSchema>;
 
 export default {
   PORT: env.PORT,
-  MONGODB_URI: env.MONGODB_URI,
   NODE_ENV: env.NODE_ENV,
   logs: {
     level: process.env.LOG_LEVEL || 'silly',
